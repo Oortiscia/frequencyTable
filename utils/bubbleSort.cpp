d42db72f1157b22e23b08efd67ed7c71c63ee8fd
oorphan <fshz1382@gmail.com>
2026-06-27 00:09:59 +0330

    verbosity
    seperated bubble sort function from the main file and created utils.

---
+++ utils/bubbleSort.cpp
@@ -0,0 +1,23 @@
+
+// Bubble sort
+void bubbleSort(int numberList[], int arrSize) {
+
+	int temp{0};
+
+
+	for (int i= 0; i < arrSize; i += 1) {
+		bool swapped{false};
+		for (int j = 0; j < (arrSize) -i -1; j +=1){
+
+			if (numberList[j]> numberList[j+1]) {
+
+				temp = numberList[j+1];
+				numberList[j+1] = numberList[j];
+				numberList[j]= temp;
+				swapped = true;
+			}
+		}
+
+		if (!swapped) {break;}	
+	}
+}
---
+++ utils/bubbleSort.hpp
@@ -0,0 +1,6 @@
+#ifndef BUBBLE_SORT_HPP
+#define BUBBLE_SORT_HPP
+
+void bubbleSort(int numberList[], int arrSize);
+
+#endif
